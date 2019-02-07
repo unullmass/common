0c04162399f3f34bbdda292b9001be8922e0f8eb
HarshithaGowda <harshitha.m.gowda@intel.com>
2019-02-07 02:09:23 -0800

    Struct changes with modifications to Offset and enc alg size variables

--- model/encryption_header.go
+++ model/encryption_header.go
@@ -2,8 +2,8 @@ package model
 
 // EncryptionHeader is used append to the encrypted file during AES GCM encryption mode
 type EncryptionHeader struct {
-	MagicText [12]byte
-	Offset    uint32
-	IV        [12]byte
-	Algorithm [6]byte
+	MagicText            [12]byte
+	OffsetInLittleEndian uint32
+	IV                   [12]byte
+	EncryptionAlgorithm  [8]byte
 }
