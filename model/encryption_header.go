855764977f6c138b06f135bffdc2709d87752447
HarshithaGowda <harshitha.m.gowda@intel.com>
2019-02-07 14:03:37 -0800

    Added version in encryption header

--- model/encryption_header.go
+++ model/encryption_header.go
@@ -4,6 +4,7 @@ package model
 type EncryptionHeader struct {
 	MagicText            [12]byte
 	OffsetInLittleEndian uint32
+	Version              [4]byte
 	IV                   [12]byte
-	EncryptionAlgorithm  [8]byte
+	EncryptionAlgorithm  [12]byte
 }
