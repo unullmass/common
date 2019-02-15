1c0e050795fe20f823aaba1565581779675ab373
HarshithaGowda <harshitha.m.gowda@intel.com>
2019-02-14 17:37:58 -0800

    Code review comments fixed

--- crypt/encryption_header.go
+++ crypt/encryption_header.go
@@ -1,5 +1,10 @@
 package crypt
 
+import (
+	"io/ioutil"
+	"strings"
+)
+
 // EncryptionHeader is used append to the encrypted file during AES GCM encryption mode
 type EncryptionHeader struct {
 	MagicText            [12]byte
@@ -7,4 +12,22 @@ type EncryptionHeader struct {
 	Version              [4]byte
 	IV                   [12]byte
 	EncryptionAlgorithm  [12]byte
+}
+
+// EncryptionHeaderExists method is used to check if the file is encryped and returns a boolean value.
+// TODO : move it a different package where all the ISecL specific functions are added
+func EncryptionHeaderExists(encFilePath string) (bool, error) {
+
+	var encryptionHeader EncryptionHeader
+	//check if file is encrypted
+	encFileContents, err := ioutil.ReadFile(encFilePath)
+	if err != nil {
+		return false, err
+	}
+
+	magicText := encFileContents[:len(encryptionHeader.MagicText)]
+	if !strings.Contains(string(magicText), EncryptionHeaderMagicText) {
+		return false, nil
+	}
+	return true, nil
 }
--- crypt/encryption_header_test.go
+++ crypt/encryption_header_test.go
@@ -1,20 +1,20 @@
-package os
+package crypt
 
 import (
 	"testing"
 	"github.com/stretchr/testify/assert"
 )
 
-func TestIsFileEncryptedTrue(t *testing.T) {
+func TestEncryptionHeaderExistsTrue(t *testing.T) {
 	encImagePath := "../test/cirros-x86.qcow2_enc"
-	isImageEncrypted, err := IsFileEncrypted(encImagePath)
+	isImageEncrypted, err := EncryptionHeaderExists(encImagePath)
 	assert.NoError(t, err)
 	assert.True(t, isImageEncrypted)
 }
 
-func TestIsFileEncryptedFalse(t *testing.T) {
+func TestEncryptionHeaderExistsFalse(t *testing.T) {
 	encImagePath := "../test/cirros-x86.qcow2"
-	isImageEncrypted, err := IsFileEncrypted(encImagePath)
+	isImageEncrypted, err := EncryptionHeaderExists(encImagePath)
 	assert.NoError(t, err)
 	assert.False(t, isImageEncrypted)
 }
