cd0b12fe2a3071b86a8eb4838eb5f257ce9ec817
Ghosh, Arijit <arijit.ghosh@intel.com>
2019-03-17 23:27:45 -0700

    V1.0/features/isecl 3439

---
+++ pkg/instance/manifest.go
@@ -0,0 +1,19 @@
+package instance
+
+/**
+ *
+ * @author purvades
+ *
+ */
+
+type Manifest struct {
+	InstanceInfo           Info `json:"instance_info"`
+	ImageEncrypted         bool `json:"image_encrypted"`
+	ImageIntegrityEnforced bool `json:"image_integrity_enforced,omitempty"`
+}
+
+type Info struct {
+	InstanceID       string `json:"instance_id"`
+	HostHardwareUUID string `json:"host_hardware_uuid"`
+	ImageID          string `json:"image_id"`
+}
