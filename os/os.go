48d1dfaac1fac708c58ccedbb491e7a441dfafb0
Vinil Cheeramvelil <vinil.cheeramvelil@intel.com>
2019-05-04 22:29:44 -0700

    add some common function for math and copying file

--- os/os.go
+++ os/os.go
@@ -18,3 +18,25 @@ func ChownR(path string, uid, gid int) error {
 		return err
 	})
 }
+
+// Copy the src file to dst. Any existing file will be overwritten and will not
+// copy file attributes.
+func Copy(src, dst string) error {
+    in, err := os.Open(src)
+    if err != nil {
+        return err
+    }
+    defer in.Close()
+
+    out, err := os.Create(dst)
+    if err != nil {
+        return err
+    }
+    defer out.Close()
+
+    _, err = io.Copy(out, in)
+    if err != nil {
+        return err
+    }
+    return out.Close()
+}
