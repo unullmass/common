8e7a54f1344245aa66c7641343b25c4201db29b6
Gowda, Harshitha M <harshitha.m.gowda@intel.com>
2019-07-03 06:15:50 -0700

    V1.0/fixes/add verify cert by sha256 method

--- tls/tls_test.go
+++ tls/tls_test.go
@@ -7,6 +7,7 @@ package tls
 import (
 	"crypto/rand"
 	"crypto/rsa"
+	"crypto/sha256"
 	"crypto/sha512"
 	"crypto/tls"
 	"crypto/x509"
@@ -20,7 +21,7 @@ import (
 	"time"
 )
 
-func TestVerifyCertBySha384(t *testing.T) {
+func createCert() ([]byte, *rsa.PrivateKey, error) {
 	ca := &x509.Certificate{
 		SerialNumber: big.NewInt(1653),
 		Subject: pkix.Name{
@@ -42,6 +43,16 @@ func TestVerifyCertBySha384(t *testing.T) {
 	pub := &priv.PublicKey
 	cert, _ := x509.CreateCertificate(rand.Reader, ca, ca, pub, priv)
 
+	return cert, priv, nil
+}
+
+func TestVerifyCertBySha384(t *testing.T) {
+
+	cert, priv, err := createCert()
+	if err != nil {
+		t.Fatal(err)
+	}
+
 	certFile, _ := ioutil.TempFile("", "cert.crt")
 	defer os.Remove(certFile.Name())
 	keyFile, _ := ioutil.TempFile("", "key.key")
@@ -51,13 +62,12 @@ func TestVerifyCertBySha384(t *testing.T) {
 	pem.Encode(keyFile, &pem.Block{Type: "RSA PRIVATE KEY", Bytes: x509.MarshalPKCS1PrivateKey(priv)})
 	keyFile.Close()
 
-	certDigest := sha512.Sum384(cert)
-
 	http.HandleFunc("/foo", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("bar"))
 	})
 	go http.ListenAndServeTLS(":1337", certFile.Name(), keyFile.Name(), nil)
 
+	certDigest := sha512.Sum384(cert)
 	tlsConfig := tls.Config{
 		InsecureSkipVerify:    true,
 		VerifyPeerCertificate: VerifyCertBySha384(certDigest),
@@ -67,6 +77,50 @@ func TestVerifyCertBySha384(t *testing.T) {
 	}
 	client := &http.Client{Transport: &transport}
 	rsp, err := client.Get("https://localhost:1337/foo")
+	if err != nil {
+		t.Fatal(err)
+	}
+	rspBytes, err := ioutil.ReadAll(rsp.Body)
+	if err != nil {
+		t.Fatal(err)
+	}
+	rspString := string(rspBytes)
+	if rspString != "bar" {
+		t.Fail()
+	}
+}
+
+func TestVerifyCertBySha256(t *testing.T) {
+
+	cert, priv, err := createCert()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	certFile, _ := ioutil.TempFile("", "cert.crt")
+	defer os.Remove(certFile.Name())
+	keyFile, _ := ioutil.TempFile("", "key.key")
+	defer os.Remove(keyFile.Name())
+	pem.Encode(certFile, &pem.Block{Type: "CERTIFICATE", Bytes: cert})
+	certFile.Close()
+	pem.Encode(keyFile, &pem.Block{Type: "RSA PRIVATE KEY", Bytes: x509.MarshalPKCS1PrivateKey(priv)})
+	keyFile.Close()
+
+	http.HandleFunc("/foobar", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("bar"))
+	})
+	go http.ListenAndServeTLS(":1338", certFile.Name(), keyFile.Name(), nil)
+
+	certDigest := sha256.Sum256(cert)
+	tlsConfig := tls.Config{
+		InsecureSkipVerify:    true,
+		VerifyPeerCertificate: VerifyCertBySha256(certDigest),
+	}
+	transport := http.Transport{
+		TLSClientConfig: &tlsConfig,
+	}
+	client := &http.Client{Transport: &transport}
+	rsp, err := client.Get("https://localhost:1338/foobar")
 	if err != nil {
 		t.Fatal(err)
 	}
