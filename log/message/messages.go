3abce4e2d2d207ac18bf847ab2b6160312121da5
Cheng, Hsiang-yu <hsiang-yu.cheng@intel.com>
2019-11-22 13:20:01 -0800

    add standard logging message strings

---
+++ log/message/messages.go
@@ -0,0 +1,35 @@
+package message
+
+var (
+	InvalidInputProtocolViolation = "input validation failed: protocol violation"
+	InvalidInputBadEncoding       = "input validation failed: unacceptable encodings"
+	InvalidInputBadParam          = "input validation failed: invalid parameter names"
+	OutputFailed                  = "failed to generate output"
+
+	TLSConnectFailed = "backend TLS connection failed"
+
+	AuthenticationSuccess = "user authenticated"
+	AuthenticationFailed  = "user authentication failed"
+	AuthorizedAccess      = "authorized request"
+	UnauthorizedAccess    = "unauthorized request"
+
+	AppRuntimeErr      = "application errors and system events: syntax and runtime errors"
+	BadConnection      = "application errors and system events: connectivity problems"
+	PerformanceProblem = "application errors and system events: performance issues"
+
+	ConfigChanged = "application errors and system events: config changed"
+	ServiceStart  = "service start"
+	ServiceStop   = "service stop"
+	LogInit       = "log init"
+
+	UserAdded         = "user added"
+	UserDeleted       = "user deleted"
+	PrivilegeModified = "privilege modified"
+	TokenIssued       = "token generated for user"
+	TokenModified     = "token modified"
+	SU                = "using systems administrative privileges"
+
+	EncKeyUsed = "using data encrypting keys"
+	DataImport = "data imported"
+	DataExport = "data exported"
+)
