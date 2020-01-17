1f9ca3330198b2fc9b99cf02a19785891ce326e7
Ghosh, Arijit <arijit.ghosh@intel.com>
2020-01-16 22:40:35 -0800

    Changes for roles and permissions

--- auth/auth.go
+++ auth/auth.go
@@ -9,24 +9,46 @@ import (
 	"strings"
 )
 
-func ValidatePermissionAndGetRoleContext(privileges []types.RoleInfo, reqRoles []types.RoleInfo,
-	retNilCtxForEmptyCtx bool) (*map[string]types.RoleInfo, bool) {
+func ValidatePermissionAndGetRoleContext(privileges []types.PermissionInfo, reqPermissions types.PermissionInfo,
+	retNilCtxForEmptyCtx bool) (*map[string]types.PermissionInfo, bool) {
 
-	ctx := make(map[string]types.RoleInfo)
-	foundMatchingRole := false
-	for _, role := range privileges {
-		for _, reqRole := range reqRoles {
-			if role.Service == reqRole.Service && role.Name == reqRole.Name {
-				if strings.TrimSpace(role.Context) == "" && retNilCtxForEmptyCtx == true {
-					return nil, true
+	ctx := make(map[string]types.PermissionInfo)
+	for _, permission := range privileges {
+		if reqPermissions.Service == permission.Service {
+			for _, rule := range permission.Rules {
+				for reqRuleIndex, reqRule := range reqPermissions.Rules {
+					if isAuthorized(rule, reqRule) {
+						if len(reqPermissions.Rules) > 1 {
+							reqPermissions.Rules[reqRuleIndex] = reqPermissions.Rules[len(reqPermissions.Rules) - 1]
+							reqPermissions.Rules = reqPermissions.Rules[:len(reqPermissions.Rules) - 1]
+						} else {
+							if strings.TrimSpace(permission.Context) == "" && retNilCtxForEmptyCtx == true {
+								return nil, true
+							} else if strings.TrimSpace(permission.Context) != "" {
+								ctx[strings.TrimSpace(permission.Context)] = permission
+								return &ctx, true
+							}
+						}
+					}
 				}
-				if strings.TrimSpace(role.Context) != "" {
-					ctx[strings.TrimSpace(role.Context)] = role
-				}
-				foundMatchingRole = true
 			}
 		}
-
 	}
-	return &ctx, foundMatchingRole
+	return &ctx, false
+}
+
+func isAuthorized(rule string, reqPermission string) bool {
+	splitRule := strings.Split(rule, ":")
+	splitReqPermission := strings.Split(reqPermission, ":")
+	if len(splitRule) < 2 {
+		return false
+	}
+	if len(splitRule) > 2 && splitRule[2] != "*" {
+		return false
+	}
+	if (splitRule[0] == "*" || splitRule[0] == splitReqPermission[0]) && (splitRule[1] == "*" || splitRule[1] == splitReqPermission[1]) {
+		return true
+	} else {
+		return false
+	}
 }
